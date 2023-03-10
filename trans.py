757727bd0dc1ab89c879586edfe70cbfbff35bba
majidhd001 <87666707+majidhd001@users.noreply.github.com>
2023-03-10 13:36:41 +0330

    Update trans.py
    Refactor: add voice tanslate

--- trans.py
+++ trans.py
@@ -1,5 +1,25 @@
 import sys
 from googletrans import Translator
+import pyttsx3
+
+
+#sudo apt-get install espeak libespeak-dev libportaudio2
+# initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+# set the voice properties
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'Persian+English-US') # you can choose any available voice
+
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate - 50)
+
+#for voice in voices:
+#    print("Voice ID:", voice.id)
+#    print("Name:", voice.name)
+#    print("Languages:", voice.languages)
+#    print("Gender:", voice.gender)
+#    print("Age:", voice.age)
 
 def translate_word(word):
     # Instantiates a translator
@@ -14,4 +34,15 @@ if __name__ == '__main__':
     args = [arg for arg in sys.argv[1:]]
     string = ' '.join(args)
     translate_word(string)
+    # convert the text to speech
+    engine.say(string)
+    engine.runAndWait()
+    
+    
+
+
+
+
+
+
 

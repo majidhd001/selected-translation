5a6fd5daf2583fa8ab47f51857a56aea8a6e4ed1
majidhd001 <87666707+majidhd001@users.noreply.github.com>
2023-03-12 12:55:58 +0330

    Refactor: trans.py
    Refacotor : I added the Google speech engine

--- trans.py
+++ trans.py
@@ -1,25 +1,8 @@
 import sys
 from googletrans import Translator
-import pyttsx3
-
-
-#sudo apt-get install espeak libespeak-dev libportaudio2
-# initialize the text-to-speech engine
-engine = pyttsx3.init()
-
-# set the voice properties
-voices = engine.getProperty('voices')
-engine.setProperty('voice', 'Persian+English-US') # you can choose any available voice
-
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate - 50)
-
-#for voice in voices:
-#    print("Voice ID:", voice.id)
-#    print("Name:", voice.name)
-#    print("Languages:", voice.languages)
-#    print("Gender:", voice.gender)
-#    print("Age:", voice.age)
+from gtts import gTTS
+import os
+from playsound import playsound
 
 def translate_word(word):
     # Instantiates a translator
@@ -34,9 +17,21 @@ if __name__ == '__main__':
     args = [arg for arg in sys.argv[1:]]
     string = ' '.join(args)
     translate_word(string)
+    
     # convert the text to speech
-    engine.say(string)
-    engine.runAndWait()
+    tts = gTTS(text=string, lang='en')
+    
+    # create a temporary file to store the speech
+    temp_file = "temp.mp3"
+    tts.save(temp_file)
+    
+    # play the speech
+    playsound(temp_file)
+
+    # delete the temporary file
+    os.remove(temp_file)
+    
+
     
     
 
